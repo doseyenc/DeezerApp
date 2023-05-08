4bd9790d84d3e83cd6ef77802633bde5975b6770
Çağrı <doseyenc@gmail.com>
2023-05-09 01:22:12 +0300

    model classes added to domain layout

---
+++ app/src/main/java/com/example/deezerapp/categories/domain/model/CategoriesData.kt
@@ -0,0 +1,5 @@
+package com.example.deezerapp.categories.domain.model
+
+data class CategoriesData(
+    val data : List<CategoriesDataData>
+)
---
+++ app/src/main/java/com/example/deezerapp/categories/domain/model/CategoriesDataData.kt
@@ -0,0 +1,12 @@
+package com.example.deezerapp.categories.domain.model
+
+data class CategoriesDataData(
+    val id: Int,
+    val name: String,
+    val picture: String,
+    val pictureBig: String,
+    val pictureMedium: String,
+    val pictureSmall: String,
+    val pictureXl: String,
+    val type: String
+)
