77238cdaf4688c177cc405318634ea6c074ed321
Çağrı <doseyenc@gmail.com>
2023-05-09 01:52:14 +0300

    categories fragment and loading anim add

---
+++ app/src/main/java/com/example/deezerapp/categories/ui/CategoriesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.deezerapp.categories.ui
+
+
+import com.example.deezerapp.common.view.BaseFragment
+import com.example.deezerapp.databinding.FragmentCategoriesBinding
+
+
+class CategoriesFragment : BaseFragment<FragmentCategoriesBinding>() {
+    override fun init() {
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupView() {
+
+    }
+
+    private fun setupViewModel() {
+
+    }
+
+}
