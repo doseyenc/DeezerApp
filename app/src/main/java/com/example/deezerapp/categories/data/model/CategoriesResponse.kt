061a1acfcea17432ee327913459459a92e5fd1f6
Çağrı <doseyenc@gmail.com>
2023-05-09 01:03:05 +0300

    data classes created for categories response

---
+++ app/src/main/java/com/example/deezerapp/categories/data/model/CategoriesResponse.kt
@@ -0,0 +1,9 @@
+package com.example.deezerapp.categories.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CategoriesResponse(
+    @SerializedName("data")
+    val data: List<CategoriesResponseData>,
+
+)
