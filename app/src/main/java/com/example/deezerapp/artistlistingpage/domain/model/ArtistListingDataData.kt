96390a2239e1710da9bb2e8907aa090ea2e66f77
Çağrı <doseyenc@gmail.com>
2023-05-09 20:39:28 +0300

    model classes created for domain layer

---
+++ app/src/main/java/com/example/deezerapp/artistlistingpage/domain/model/ArtistListingDataData.kt
@@ -0,0 +1,13 @@
+package com.example.deezerapp.artistlistingpage.domain.model
+
+data class ArtistListingDataData(
+    val id: Int?,
+    val name: String?,
+    val picture: String?,
+    val picture_small: String?,
+    val picture_medium: String?,
+    val picture_big: String?,
+    val picture_xl: String?,
+    val trackList: String?,
+    val type: String?
+)
