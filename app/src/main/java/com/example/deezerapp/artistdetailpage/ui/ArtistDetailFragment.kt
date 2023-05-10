6dc3a69b030bb546b21aa115cd0249819c3ead64
Çağrı <doseyenc@gmail.com>
2023-05-10 22:21:33 +0300

    artist detail frgament added

---
+++ app/src/main/java/com/example/deezerapp/artistdetailpage/ui/ArtistDetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.deezerapp.artistdetailpage.ui
+
+
+import com.example.deezerapp.common.view.BaseFragment
+import com.example.deezerapp.databinding.FragmentArtistDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ArtistDetailFragment : BaseFragment<FragmentArtistDetailBinding>() {
+    override fun init() {
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+
+    }
+
+    private fun setupView() {
+
+
+    }
+
+}
