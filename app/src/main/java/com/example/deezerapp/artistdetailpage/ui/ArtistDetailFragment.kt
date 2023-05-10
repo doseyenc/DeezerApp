c3655a0f5252440c5c1c1cd19db825af619430c4
Çağrı <doseyenc@gmail.com>
2023-05-11 00:39:39 +0300

    album listing done

--- app/src/main/java/com/example/deezerapp/artistdetailpage/ui/ArtistDetailFragment.kt
+++ app/src/main/java/com/example/deezerapp/artistdetailpage/ui/ArtistDetailFragment.kt
@@ -4,23 +4,37 @@ package com.example.deezerapp.artistdetailpage.ui
 import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.deezerapp.artistdetailpage.domain.model.AlbumListingData
 import com.example.deezerapp.artistdetailpage.domain.model.ArtistDetailData
+import com.example.deezerapp.artistdetailpage.ui.adapter.AlbumListingAdapter
+import com.example.deezerapp.artistdetailpage.ui.viewmodel.AlbumListingViewModel
 import com.example.deezerapp.artistdetailpage.ui.viewmodel.ArtistDetailViewModel
+import com.example.deezerapp.artistdetailpage.ui.viewstate.AlbumListingViewState
 import com.example.deezerapp.artistdetailpage.ui.viewstate.ArtistDetailViewState
+import com.example.deezerapp.artistlistingpage.domain.model.ArtistListingDataData
+import com.example.deezerapp.artistlistingpage.ui.adapter.ArtistListingAdapter
 import com.example.deezerapp.artistlistingpage.ui.viewstate.ArtistListingViewState
 import com.example.deezerapp.common.util.createPlaceHolder
 import com.example.deezerapp.common.util.setImage
 import com.example.deezerapp.common.view.BaseFragment
 import com.example.deezerapp.databinding.FragmentArtistDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class ArtistDetailFragment : BaseFragment<FragmentArtistDetailBinding>() {
     //ViewModel
     private val artistDetailViewModel: ArtistDetailViewModel by viewModels()
+    private val albumListingViewModel: AlbumListingViewModel by viewModels()
 
     //args
     private val args: ArtistDetailFragmentArgs by navArgs()
+
+    //Adapter
+    @Inject
+    lateinit var albumListingAdapter: AlbumListingAdapter
     override fun init() {
         setupView()
         setupViewModel()
@@ -31,11 +45,50 @@ class ArtistDetailFragment : BaseFragment<FragmentArtistDetailBinding>() {
             getArtistDetail(
                 id = args.ArtistListingDataData.id.toString(),
             )
-            getStateLiveData().observe(viewLifecycleOwner){
+            getStateLiveData().observe(viewLifecycleOwner) {
                 renderGetArtistDetailStatusViewState(it)
             }
 
         }
+        with(albumListingViewModel) {
+            getAlbumList(
+                id = args.ArtistListingDataData.id.toString(),
+            )
+            getStateLiveData().observe(viewLifecycleOwner) {
+                renderGetAlbumListingStatusViewState(it)
+            }
+        }
+    }
+
+    private fun renderGetAlbumListingStatusViewState(viewState: AlbumListingViewState) =
+        when (viewState) {
+            is AlbumListingViewState.Loading -> loadingInProgressAlbumListing()
+            is AlbumListingViewState.Empty -> emptyStateAlbumListing()
+            is AlbumListingViewState.Success -> displayAlbumListingData(viewState.albumListingData)
+            is AlbumListingViewState.Error -> errorHandleAlbumListing(viewState.throwable)
+        }
+
+    private fun errorHandleAlbumListing(throwable: Throwable) {
+        binding.artistDetailStateLayout.loading()
+        Log.e("ArtistDetailFragment", "errorHandle: ${throwable.message}")
+    }
+
+    private fun displayAlbumListingData(albumListingData: AlbumListingData?) {
+        Log.e("ArtistDetailFragment", "displayData: $albumListingData")
+        with(binding) {
+            artistDetailStateLayout.content()
+            albumListingData?.data?.let { albumListingAdapter.setItems(it) }
+        }
+    }
+
+    private fun emptyStateAlbumListing() {
+        binding.artistDetailStateLayout.loading()
+        Log.e("ArtistDetailFragment", "emptyState: ")
+    }
+
+    private fun loadingInProgressAlbumListing() {
+        binding.artistDetailStateLayout.loading()
+        Log.e("ArtistDetailFragment", "loading: ")
     }
 
     private fun renderGetArtistDetailStatusViewState(viewState: ArtistDetailViewState) =
@@ -75,8 +128,24 @@ class ArtistDetailFragment : BaseFragment<FragmentArtistDetailBinding>() {
     }
 
     private fun setupView() {
-
-
+        setUpRv()
+        albumListingAdapter.onAlbumClick = {
+            Log.e("ArtistDetailFragment", "onAlbumClick: $it")
+        }
     }
+
+    private fun setUpRv() {
+        albumListingAdapter = AlbumListingAdapter()
+        with(binding.recyclerViewArtistAlbums) {
+            layoutManager =
+                LinearLayoutManager(
+                    requireContext(),
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
+            adapter = albumListingAdapter
+        }
+    }
+
 
 }
