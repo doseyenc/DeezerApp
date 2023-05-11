7af1b4ef8ae4414e7965b6145dc189df236c3003
Çağrı <doseyenc@gmail.com>
2023-05-12 00:18:53 +0300

    Domain data classes created

---
+++ app/src/main/java/com/example/deezerapp/albumdetailpage/domain/model/TrackDomainDataData.kt
@@ -0,0 +1,10 @@
+package com.example.deezerapp.albumdetailpage.domain.model
+
+data class TrackDomainDataData(
+    val id : Int?,
+    val title : String?,
+    val titleShort : String?,
+    val link : String?,
+    val duration : Int?,
+    val preview : String?,
+)
