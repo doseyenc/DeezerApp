d488da572fd1d5ca4be43d47192c2b1aecc25748
Çağrı <doseyenc@gmail.com>
2023-05-12 23:25:12 +0300

    artist detail page parameter type changed

--- app/src/main/java/com/example/deezerapp/artistdetailpage/data/model/ArtistDetailResponse.kt
+++ app/src/main/java/com/example/deezerapp/artistdetailpage/data/model/ArtistDetailResponse.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class ArtistDetailResponse(
     @SerializedName("id")
-    val id: Int?,
+    val id: Long?,
 
     @SerializedName("name")
     val name: String?,
--- app/src/main/java/com/example/deezerapp/artistdetailpage/domain/model/AlbumListingDataData.kt
+++ app/src/main/java/com/example/deezerapp/artistdetailpage/domain/model/AlbumListingDataData.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class AlbumListingDataData(
-    val id: Int?,
+    val id: Long?,
     val title: String?,
     val cover: String?,
     val coverSmall: String?,
