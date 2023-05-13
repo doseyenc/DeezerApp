920c72bafc2c07d58a6e4a1a878a3d54b6a02599
Çağrı <doseyenc@gmail.com>
2023-05-13 22:56:19 +0300

    splash screen added

--- app/src/main/java/com/example/deezerapp/MainActivity.kt
+++ app/src/main/java/com/example/deezerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.deezerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.deezerapp.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ import dagger.hilt.android.HiltAndroidApp
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
